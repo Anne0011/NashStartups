ec9ee343fad6c235a4a91ba93533a26291223aae
Ryan Macy <ryan.w.macy@gmail.com>
2015-06-24 22:05:00 -0500

    fix investors controller

--- js/controllers/investorsCtrl.js
+++ js/controllers/investorsCtrl.js
@@ -1,17 +1,3 @@
-myApp.controller('investorsCtrl', ['$scope','nash', '$firebaseArray' function($scope, nash, $firebaseArray) {
- {
-    var list = $firebaseArray(new Firebase(URL));
-
-    // add an item
-    list.$add({ foo: "bar" }).then(...);
-
-    // remove an item
-    list.$remove(2).then(...);
-
-    // make the list available in the DOM
-    $scope.list = list;
-  }
-]);
-
-  console.log("investor");
+myApp.controller('investorsCtrl', ['$scope', 'nash', '$firebaseArray', function($scope, nash, $firebaseArray) {
+    $scope.firms = $firebaseArray(new Firebase("https://nashstartuplist.firebaseio.com/investor_firms"));
 }]);
